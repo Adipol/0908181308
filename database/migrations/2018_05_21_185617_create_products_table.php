5d60612997ab01b8883624383e94221d4e2a078c
george <adipol13@gmail.com>
2018-05-21 19:15:35 -0400

    creacion de tablas productos y categorias

---
+++ database/migrations/2018_05_21_185617_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+			$table->increments('id');
+
+			$table->unsignedInteger('category_id');
+			$table->foreign('category_id')->references('id')->on('categories');
+			
+			$table->string('name',50)->unique();
+			$table->string('description',250)->nullable();
+			$table->integer('stock');
+			$table->boolean('condition')->default(1);
+			$table->timestamps();
+			$table->integer('ucm');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
